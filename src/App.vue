6d7c97db63e7d45e6a58549da9a312a2c05de2ab
DavideAtzo <atzo9477@gmail.com>
2023-03-23 18:04:19 +0100

    fine esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
-<script setup>
-
+<script>
+  import AppTitleVue from "./components/AppTitle.vue";
+  export default {
+    components: {
+      AppTitleVue
+    }
+  }
 </script>
 
 <template>
+    <AppTitleVue></AppTitleVue>
 </template>
 
 <style scoped>
